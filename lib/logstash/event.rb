24a404ab6534b97f5b0749e3b39a41f3145f6ff4
William Pratt <wpratt@demandbase.com>
2013-12-19 14:24:03 -0800

    wrap to_json in a rescue

--- lib/logstash/event.rb
+++ lib/logstash/event.rb
@@ -30,7 +30,7 @@ end
 # * "@version" - the version of the schema. Currently "1"
 #
 # They are prefixed with an "@" symbol to avoid clashing with your
-# own custom fields. 
+# own custom fields.
 #
 # When serialized, this is represented in JSON. For example:
 #
@@ -53,7 +53,7 @@ class LogStash::Event
         data["@timestamp"] = Time.parse(t).gmtime
       end
     else
-      data["@timestamp"] = ::Time.now.utc 
+      data["@timestamp"] = ::Time.now.utc
     end
     data["@version"] = "1" if !@data.include?("@version")
   end # def initialize
@@ -107,7 +107,7 @@ class LogStash::Event
   def ruby_timestamp
     raise DeprecatedMethod
   end # def unix_timestamp
-  
+
   # field-related access
   public
   def [](str)
@@ -116,7 +116,7 @@ class LogStash::Event
       return LogStash::Util::FieldReference.exec(str, @data)
     end
   end # def []
-  
+
   public
   def []=(str, value)
     if str == "@timestamp" && !value.is_a?(Time)
@@ -155,10 +155,13 @@ class LogStash::Event
   def fields
     raise DeprecatedMethod
   end
-  
+
   public
   def to_json(*args)
-    return @data.to_json(*args) 
+    begin
+      return @data.to_json(*args)
+    rescue
+    end
   end # def to_json
 
   def to_hash
@@ -192,7 +195,7 @@ class LogStash::Event
   end # def remove
 
   # sprintf. This could use a better method name.
-  # The idea is to take an event and convert it to a string based on 
+  # The idea is to take an event and convert it to a string based on
   # any format values, delimited by %{foo} where 'foo' is a field or
   # metadata member.
   #
@@ -205,7 +208,7 @@ class LogStash::Event
   # If a %{name} value is an array, then we will join by ','
   # If a %{name} value does not exist, then no substitution occurs.
   #
-  # TODO(sissel): It is not clear what the value of a field that 
+  # TODO(sissel): It is not clear what the value of a field that
   # is an array (or hash?) should be. Join by comma? Something else?
   public
   def sprintf(format)
